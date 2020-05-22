9acc350545310fac866eb46debf72de29e24109b
aharral09 <aharral09@gmail.com>
2020-05-22 17:45:57 -0400

    Made calculator folder

---
+++ Calculator/Calculator.py
@@ -0,0 +1,33 @@
+# Python Calculator
+# Author Alex Harral
+num1 = int(input ("Enter in first number: "))
+
+operation = input('''
+        Please type in the math operation you want to preform:
+
++ for addition
+- for subtraction
+/ for division
+* for multiplication
+''')
+
+num2 = int(input ("Enter in second number: "))
+
+if operation == '+':
+    print('{} + {}'.format(num1, num2))
+    print(num1 + num2)
+    
+
+if operation == '-':
+    print('{} - {}'.format(num1, num2))
+    print(num1 - num2)
+
+
+if operation == '/':
+    print('{} / {}'.format(num1, num2))
+    print(num1 / num2)
+
+
+if operation == '*':
+    print('{} * {}'.format(num1, num2))
+    print(num1 * num2)
