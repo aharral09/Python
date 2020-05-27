b8bb3ba229c2e1df3387ca4950fe5f405d139e2f
alex harral <aharral09@gmail.com>
2020-05-26 21:57:31 -0400

    ifElse.py was created along with Hackerrank folder for solving hackerrank problems

---
+++ HackerRank/README.md
@@ -0,0 +1,1 @@
+This is a folder dedicated to problem solving hackerrank problems
---
+++ HackerRank/ifElse.py
@@ -0,0 +1,11 @@
+if n % 2 != 0:
+    print "Weird"
+
+elif n % 2 == 0 && (n >= 2) && (n <= 5):
+    print "Not Weird"
+
+elif n % 2 == 0 && (n >= 6) && (n <= 20):
+    print "Weird"
+
+else print "Not Weird"
+
