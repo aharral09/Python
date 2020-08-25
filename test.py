d8aca3a637a39d5dfeeea56c83b6f40354e9cc75
Alex Harral <aharral09@gmail.com>
2020-08-24 23:28:26 -0400

    trying out pygame

---
+++ test.py
@@ -0,0 +1,33 @@
+import sys, pygame
+pygame.init()
+
+size = width, height = 1366, 738
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+self = pygame.image.load("index.png")
+self.rect = self.image.get_rect()
+self.screen_rect = screen.get_rect()
+
+self.image.set_colorkey((255, 255, 255))
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
+def random_color():
+    rgbl=[255,0,0]
+    random.shuffle(rgbl)
+    return tuple(rgbl)
